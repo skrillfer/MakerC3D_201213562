f062be6b4cc3863fe160ceab070d4cab364513a0
skrillfer <rluis4490@gmail.com>
2017-03-08 23:52:41 -0600

    Hoja Trabajo 3 201213562 Luis Fernando Ramirez Santos

--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,94 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
-
+    private String etiquetaTrue;
+    private String etiquetaFalse;
+    private String etiquetaTrueAux;
+    private String etiquetaFalseAux;
+    private String textosAux;    
     public NodoC3D(String cad) {
         this.cad = cad;
     }
     
     public String getCad(){
         return cad;
+    }
+
+    /**
+     * @param cad the cad to set
+     */
+    public void setCad(String cad) {
+        this.cad = cad;
+    }
+
+    /**
+     * @return the etiquetaTrue
+     */
+    public String getEtiquetaTrue() {
+        return etiquetaTrue;
+    }
+
+    /**
+     * @param etiquetaTrue the etiquetaTrue to set
+     */
+    public void setEtiquetaTrue(String etiquetaTrue) {
+        this.etiquetaTrue = etiquetaTrue;
+    }
+
+    /**
+     * @return the etiquetaFalse
+     */
+    public String getEtiquetaFalse() {
+        return etiquetaFalse;
+    }
+
+    /**
+     * @param etiquetaFalse the etiquetaFalse to set
+     */
+    public void setEtiquetaFalse(String etiquetaFalse) {
+        this.etiquetaFalse = etiquetaFalse;
+    }
+
+    /**
+     * @return the etiquetaTrueAux
+     */
+    public String getEtiquetaTrueAux() {
+        return etiquetaTrueAux;
+    }
+
+    /**
+     * @param etiquetaTrueAux the etiquetaTrueAux to set
+     */
+    public void setEtiquetaTrueAux(String etiquetaTrueAux) {
+        this.etiquetaTrueAux = etiquetaTrueAux;
+    }
+
+    /**
+     * @return the etiquetaFalseAux
+     */
+    public String getEtiquetaFalseAux() {
+        return etiquetaFalseAux;
+    }
+
+    /**
+     * @param etiquetaFalseAux the etiquetaFalseAux to set
+     */
+    public void setEtiquetaFalseAux(String etiquetaFalseAux) {
+        this.etiquetaFalseAux = etiquetaFalseAux;
+    }
+
+    /**
+     * @return the textosAux
+     */
+    public String getTextosAux() {
+        return textosAux;
+    }
+
+    /**
+     * @param textosAux the textosAux to set
+     */
+    public void setTextosAux(String textosAux) {
+        this.textosAux = textosAux;
     }
         
 }
